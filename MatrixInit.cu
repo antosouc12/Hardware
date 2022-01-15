e49596d18a6d2ff54f78f0a85b4f887c78d655c5
antosouc12 <56081832+antosouc12@users.noreply.github.com>
2022-01-15 18:41:58 +0100

    Update MatrixInit.cu

--- MatrixInit.cu
+++ MatrixInit.cu
@@ -1,11 +1,11 @@
-#include <curand_kernel.h>
+#include <curand_kernel.h> //Curand kernel est une library qui nous permet de generer des valeurs aleatoires a l'interieur du GPU
 
 
 __global__ void FillMatrix(float* M, int n){
 
     int idx=blockIdx.x*blockDim.x+threadIdx.x;
     curandState state;
-    curand_init((unsigned long long)clock() + idx, 0, 0, &state);
+    curand_init((unsigned long long)clock() + idx, 0, 0, &state);       //Ces deux lignes permettent d'initer le generateur aleatoire
     
     if (idx<n){
         M[idx]=curand_uniform_double(&state);
