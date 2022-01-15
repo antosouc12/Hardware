2ab0ffdbed8befb7024f07e9f4d6c3a366843084
antosouc12 <56081832+antosouc12@users.noreply.github.com>
2022-01-15 17:54:31 +0100

    Update NN.cu

--- NN.cu
+++ NN.cu
@@ -18,9 +18,7 @@ __host__ int main(){
     float *raw_data;
     raw_data=(float*)malloc(n*p*sizeof(float));
     float *M_d;
-    printf("You are at cudaMalloc \n");
     cudaMalloc((void **) &M_d,size_f*n*p);
-    printf("You are at FillMatrix \n");
     FillMatrix<<<Nblock,Nthread>>>(M_d,n*p);
 
     cudaMemcpy(raw_data,M_d,n*p*size_f,cudaMemcpyDeviceToHost);
@@ -30,18 +28,12 @@ __host__ int main(){
     float *C1_kernel;
     C1_kernel=(float *)malloc(size_f*5*5*6);
 
-    printf("You are at for loop \n");
     for(int i=0;i<6;i++){
-        printf("You are at FillMatrix 2 \n");
         FillMatrix<<<Nblock,Nthread>>>(M_d,5*5);
         cudaMemcpy(C1_kernel+i*5*5,M_d,5*5*size_f,cudaMemcpyDeviceToHost);
     }
 
     MatrixPrint(C1_kernel,5,5);
-
-    // float C1_data[6*28*28];
-    // float S1_data[6*14*14];
-
 
     float *C1_data;
     float *S1_data;
@@ -58,48 +50,18 @@ __host__ int main(){
     cudaMemcpy(C1_kernel_d,C1_kernel, 5*5*6*sizeof(float),cudaMemcpyHostToDevice);
     cudaMemcpy(M_d,raw_data,32*32*6*sizeof(float),cudaMemcpyHostToDevice);
 
-
-    // for(int i=0;i<28*28*6;i++){
-    //     C1_data[i]=(float)i;
-    // }
-
-    // for (int i=0;i<14*14*6;i++){
-    //     S1_data[i]=(float)i;
-    // }
-    // printf("You are at memset \n");
-
-    // memset(C1_data, 5, sizeof(C1_data));
-    // memset(S1_data, 5, sizeof(S1_data));
-
-    // MatrixPrint(C1_data,28,28);
-    // MatrixPrint(S1_data,14,14);
-
-    // cudaMemcpy(C1_data_d,C1_data,28*28*6*size_f,cudaMemcpyHostToDevice);
-    // cudaMemcpy(S1_data_d,S1_data,14*14*6*size_f,cudaMemcpyHostToDevice);
-
     Nblock=28;
     Nthread=28*6;
 
     //Premiere couche
-
-    // printf("You are at for loop 2 \n");
-    // for(int i=0;i<6;i++){
-    //     printf("You are at convolve 1\n");
-    //     convolve2d<<<Nblock,Nthread>>>(C1_data_d+i*28*28,M_d,C1_kernel_d+i*5*5,32,5);
-    //     cudaDeviceSynchronize();
-    //     printf("You are at convolve 2\n");
-    // }
     
     convolve2d<<<Nblock,Nthread>>>(C1_data_d,M_d,C1_kernel_d,32,5);
 
     cudaMemcpy(C1_data,C1_data_d,28*28*6*size_f,cudaMemcpyDeviceToHost);
-    //cudaFree(C1_data_d);
     
     MatrixPrint(C1_data,28,28);
     
     //Seconde couche 
-    
-    printf("You are at matmoy \n");
 
     MatMoy<<<Nblock,Nthread>>>(S1_data_d,C1_data_d,28);
 
@@ -107,9 +69,4 @@ __host__ int main(){
 
     MatrixPrint(S1_data,14,14);
 
-    printf("You are done \n");
-
-  
-
-    
 }
